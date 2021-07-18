ed302ff1cf306b5ef41c2ccdac0be066675b5c5e
brenoafb <breno.afb@gmail.com>
2021-07-18 13:24:27 -0300

    Add glyph drawing program

--- example_programs/glyph.asm
+++ example_programs/glyph.asm
@@ -1,5 +1,8 @@
 .data
 
+hello: .asciz "Hello from the program!\n"
+goodbye: .asciz "Done writing glyph\n"
+
 glyph: .byte
 0   ,0   ,0xff,0xff,0   ,0,
 0   ,0xff,0   ,0   ,0xff,0,
@@ -16,34 +19,62 @@ height: .word 128
 
 
 .text
-li a0 0x3000
+
+la a0 hello
+li a7 4       # print string
+ecall
+
+li a0 0x4000
 li a1 0
 
-call draw_glyph_line
-call next_line
-addi a1 zero 1
+addi sp sp 4
+lw a0 -4(sp)
+call draw_glyph
+lw a0 -4(sp)
+addi sp sp -4
 
-call draw_glyph_line
-call next_line
-addi a1 zero 1
+j loop
 
-call draw_glyph_line
-call next_line
-addi a1 zero 1
+lw t0 glyph_width # go to next char space
+add a0 a0 t0
 
-call draw_glyph_line
-call next_line
-addi a1 zero 1
+li a1 0
+addi sp sp 4
+lw a0 -4(sp)
+call draw_glyph
+lw a0 -4(sp)
+addi sp sp -4
 
-call draw_glyph_line
-call next_line
-addi a1 zero 1
+la a0 goodbye
+li a7 4       # print string
+ecall
+
+j loop
 
 loop:
 j loop
 
 # draw a glyph in the position starting at a0
-# draw_glyph:
+draw_glyph:
+li a1 0
+lw a2 glyph_height
+draw_glyph_loop:
+beqz a2 draw_glyph_end
+
+addi sp sp 4
+sw ra -4(sp)
+call draw_glyph_line
+lw ra -4(sp)
+addi sp sp 4
+
+
+lw t0 width 			# go to next line
+add a0 a0 t0
+addi a1 a1 1                    # next glyph line
+addi a2 a2 -1                   # decrement counter
+j draw_glyph_loop
+draw_glyph_end:
+ret
 
 # draw the a1'th line at the position pointed to
 # by a0
@@ -66,15 +97,14 @@ j draw_glyph_line_loop
 draw_glyph_line_end:
 ret
 
+
 # puts in a0 the address of the same
 # position in the next line
 next_line:
-# lw t1 width
-# lw t2 glyph_height
-# mul t1 t1 t2
-# add a0 a0 t1
-lw t0 width
-add a0 a0 t0
+lw t1 width
+lw t2 glyph_height
+mul t1 t1 t2
+add a0 a0 t1
 ret
 
 add x0 x0 x0
